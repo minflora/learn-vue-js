b18f3dc788063827869f450e4541a3d632ba8839
전지민 <117070821+jmFlora@users.noreply.github.com>
2023-02-09 17:38:01 +0900

    vue1

---
+++ vue-form/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <img :src="username" alt="">
+      <input type="password" id="password" v-model="password"/>
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data:function() {
+    return {
+      username:'',
+      password:'123',
+    }
+  },
+  methods:{
+    submitForm: function() {
+      //event.preventDefault();//form을 제어하는 방식>> v-on:submit.prevent(똑같은 역할을 한다.)
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data ={
+        username : this.username,
+        password: this.password
+      }
+      axios.post(url, data)//axios는 http통신 라이브러리, fetch(), ajax()  >> post는 데이터를 생성하거나 조작할 때 사용
+        .then(function(response){
+          console.log(response)
+        })
+        .catch(function(error){
+          console.log(error);
+        })
+  }  
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-form/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ response }}</div>
+</template>
+
+<script>
+export default {
+    props:['abc'],
+    mounted(){
+        this.$emit()
+    }
+}
+</script>
+
+<style>
+
+</style>
